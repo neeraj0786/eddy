2c0920aca7d10a8f32fa6a779454bb4a7ffabf1f
Neeraj <msbobby87@gmail.com>
2020-08-10 11:10:20 +0530

    bug

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-const express = require('express');
-const app = express();
-const server=require('http').Server(app);
-const {v4 : uuidv4} =require('uuid');
-const io = require('socket.io')(server);
-const {ExpressPeerServer}=require('peer');
-const peerServer = ExpressPeerServer(server,{
-    debug :true});
-app.use('/peerjs',peerServer);
-app.set('view engine','ejs');
-app.use(express.static('public'));
-
-app.get('/',(req,resp)=>{
-    resp.redirect(`${uuidv4()}`);
-})
-app.get('/:room',(req,resp)=>{
-    resp.render('room',{
-        roomID : req.params.room
-    })
-})
-io.on('connection',socket=>{
-    socket.on('join-room',(roomID,userID)=>{
-        socket.join(roomID);
-        socket.to(roomID).broadcast.emit('user-connected',userID);
-        socket.on('message',(message)=>{
-            io.to(roomID).emit('createMessage',message);
-        });
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-          });
-    }); 
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
 });
-server.listen(process.env.PORT||3000,console.log('Listening'));
+const { v4: uuidV4 } = require('uuid')
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT||3000)
